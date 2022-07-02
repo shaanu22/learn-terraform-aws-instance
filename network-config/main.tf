04eec2acd09fb953d8054417a52f74d73b4e9a4b
Vladimir Mukhin <Vladimir_Mukhin@epam.com>
2022-07-02 09:39:37 -0400

    Remove unneded locals

--- network-config/main.tf
+++ network-config/main.tf
@@ -19,13 +19,8 @@ resource "aws_vpc" "main" {
   }
 }
 
-locals {
-  public_cidr  = [var.public_cidr]
-  private_cidr = [var.private_cidr]
-}
-
 resource "aws_subnet" "public" {
-  count      = length(local.public_cidr)
+  count      = length(var.public_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_cidr[count.index]
 
@@ -35,7 +30,7 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count      = length(local.private_cidr)
+  count      = length(var.private_cidr)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_cidr[count.index]
 
@@ -53,12 +48,12 @@ resource "aws_internet_gateway" "main" {
 }
 
 resource "aws_eip" "nat" {
-  count = length(local.public_cidr)
+  count = length(var.public_cidr)
   vpc   = true
 }
 
 resource "aws_nat_gateway" "main" {
-  count         = length(local.public_cidr)
+  count         = length(var.public_cidr)
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
@@ -81,7 +76,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table" "private" {
-  count  = length(local.private_cidr)
+  count  = length(var.private_cidr)
   vpc_id = aws_vpc.main.id
 
   route {
@@ -95,13 +90,13 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private" {
-  count          = length(local.private_cidr)
+  count          = length(var.private_cidr)
   subnet_id      = aws_subnet.private[0].id
   route_table_id = aws_route_table.private[0].id
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(local.public_cidr)
+  count          = length(var.public_cidr)
   subnet_id      = aws_subnet.public[0].id
   route_table_id = aws_route_table.public.id
 }
