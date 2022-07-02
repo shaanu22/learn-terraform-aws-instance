cb6d22696ebe3e2cf63e1c31cec54dc738c9ef76
Vladimir Mukhin <Vladimir_Mukhin@epam.com>
2022-07-02 09:53:46 -0400

    Correct route table associations

--- network-config/main.tf
+++ network-config/main.tf
@@ -78,12 +78,12 @@ resource "aws_route_table" "private" {
 
 resource "aws_route_table_association" "private" {
   count          = length(var.private_cidr)
-  subnet_id      = aws_subnet.private[0].id
-  route_table_id = aws_route_table.private[0].id
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
 
 resource "aws_route_table_association" "public" {
   count          = length(var.public_cidr)
-  subnet_id      = aws_subnet.public[0].id
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
