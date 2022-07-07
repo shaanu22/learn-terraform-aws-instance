56345a1583b779cfa5950da3cce3b384e99c4fae
Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>
2022-07-06 21:12:44 -0400

    Update backend.tf

--- network-config/backend.tf
+++ network-config/backend.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket         = "s3-backend-bucket"
-    key            = "terraform.tfstate"
+    key            = "network-config.tfstate"
     region         = "us-east-1"
     dynamodb_table = "lock"
   }
