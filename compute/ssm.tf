c0e164e2844b40503a78585b52d6cf01d4fcb123
ADMIN <mails2samtaiwo@gmail.com>
2022-07-16 15:45:24 -0500

    Placed two policies under one IAM Role

--- compute/ssm.tf
+++ compute/ssm.tf
@@ -1,101 +1,48 @@
-resource "aws_iam_role" "SSMRole" {
+resource "aws_iam_policy" "SSMRole" {
   name = "SSMRole"
+  description = "Policy for EC2 Access to SSM"
 
-  assume_role_policy = <<EOF
+  policy = <<EOF
 {
     "Version": "2012-10-17",
-    "Statement": {
-      "Effect": "Allow",
-      "Principal": {"Service": "ssm.amazonaws.com"},
-      "Action": "sts:AssumeRole"
-   }
- }
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "ec2:Describe*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetEncryptionConfiguration"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "kms:Decrypt"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
 EOF
 }
 
-resource "aws_iam_role_policy" "SSMRole" {
-  name = "SSMRole"
-  role = aws_iam_role.SSMRole.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-
-      {
-        "Effect" : "Allow",
-        "Action" : "s3:*",
-        "Resource" : "*"
-      }
-    ]
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "SSMRole-Attach" {
-  role       = aws_iam_role.SSMRole.name
+  role       = aws_iam_role.asg-roles.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_ssm_activation" "SSM-ASG" {
-  name               = "SSM_ASG_activation"
-  description        = "SSMRole-Activation"
-  iam_role           = aws_iam_role.SSMRole.id
-  registration_limit = "5"
-  depends_on         = [aws_iam_role_policy_attachment.SSMRole-Attach]
-}
-
-/*resource "aws_ssm_document" "SSM-ASG" {
-  name          = "SSM-ASG"
-  document_type = "Automation"
-
-  content = <<DOC
-  {
-    "schemaVersion": "1.2",
-    "description": "Check ip configuration of a Linux instance.",
-    "parameters": {
-
-    },
-    "runtimeConfig": {
-      "aws:runShellScript": {
-        "properties": [
-          {
-            "id": "0.aws:runShellScript",
-            "runCommand": ["ifconfig"]
-          }
-        ]
-      }
-    }
-  }
-DOC
-}
-
-data "aws_ssm_document" "SSM-ASG" {
-  name            = aws_ssm_document.SSM-ASG.name
-  document_format = "JSON"
-}*/
-
-/*resource "aws_ssm_association" "SSM-ASG" {
-  name = data.aws_ssm_document.SSM-ASG
-
-  targets {
-    key    = "InstanceIds"
-    values = [aws_autoscaling_group.hello-devOps-asg.id]
-  }
-}*/
-
-resource "aws_iam_instance_profile" "SSM-ASG" {
-  name = "SSM-ASG"
-  role = aws_iam_role.SSMRole.name
-}
-
-resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "policy-attachment"
-  roles      = [aws_iam_role.s3fullaccess.name, aws_iam_role.SSMRole.id]
+resource "aws_iam_role_policy_attachment" "ssm-asg-attachment" {
+  role       = aws_iam_role.asg-roles.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
